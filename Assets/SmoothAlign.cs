66e22d8618101e9ef40f1c7e30d23ffc2c2c9d81
Maksim Doroshin <m1doroshin@gmail.com>
2026-02-05 05:37:59 +0300

    added interaction to NN layers

---
+++ Assets/SmoothAlign.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SmoothAlign : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Transform t = GetComponent<Transform>();
+
+        float x_torque = -t.rotation.eulerAngles.x;
+        float z_torque = -t.rotation.eulerAngles.z;
+        if(x_torque < -180) x_torque += 360;
+        if(z_torque < -180) z_torque += 360;
+
+        x_torque*=5;
+        z_torque*=5;
+
+        if(x_torque > 100) x_torque = 100;
+        if(z_torque > 100) z_torque = 100;
+        if(x_torque < -100) x_torque = -100;
+        if(z_torque < -100) z_torque = -100;
+        rb.AddRelativeTorque(new Vector3(x_torque, 0, z_torque) * Time.fixedDeltaTime);
+
+        if(t.position.y < 0.5f) t.position = new Vector3(t.position.x, 0.5f, t.position.z);
+    }
+}
